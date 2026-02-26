45ed03e5063b54b262a61da27304d4760d79073f
Pistol Axelrod <sethwilliammaravilla@gmail.com>
2026-02-27 03:12:35 +0800

    Did week 6, 7, and 8 coding homework.

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
 
-    public class Employee {
+    public static void main(String[] arg){
 
-        private String employeeName;
-        private long employeeNumber;
-        private double hoursWorked;
-        private byte clockIn;
-        private byte clockOut;
-        private double hourlyRate;
-        private double grossSalary;
-        private double netSalary;
-        private byte deduction;
+        int workedHours = 45;
+        int hoursPerDay = 8;
 
-        public Employee (String employeeName, long employeeNumber, double hourlyRate) {
-            this.employeeName = employeeName;
-            this.employeeNumber = employeeNumber;
-            this.hourlyRate = hourlyRate;
-        }
+        int totalDaysWorked = workedHours / hoursPerDay;
+        int remainingHours = workedHours % hoursPerDay;
 
-        public double getHoursWorked() {
-            return
-        }
-
-        public double getGrossSalary() {
-            return getHoursWorked() * hourlyRate;
-        }
-
-        public double getNetSalary() {
-            return getGrossSalary() / deduction;
-        }
-
-        public String getEmployeeName() {
-            return getEmployeeName();
-        }
+        System.out.println(totalDaysWorked + " Full Days Worked");
+        System.out.println(remainingHours + " Remaining Hours");
 
     }
 
-    public static void main(String[] args) {
+       public static void main(String[] arg){
 
-        String employeeName = "Willy Wonder";
-        long employeeNumber = 2_025_105_567;
-        int checkIn = 9;
-        int checkOut = 17;
-        int salary = 100;
-        byte PERCENT = 100;
-        byte deduction = 8;
+        int workedHours = 45;
+        int hoursPerDay = 8;
 
-        //check in & out times at represented by military time without 0
-        //make sure to put checkOut first to prevent making negative numbers for salary
-        int workHours = checkOut - checkIn;
-        int grossSalary = salary * workHours;
+        int totalDaysWorked = workedHours / hoursPerDay;
+        int remainingHours = workedHours % hoursPerDay;
 
-        //deductions of grossSalary
-        float netSalary = grossSalary * ((float)deduction / PERCENT);
+        System.out.println(totalDaysWorked + " Full Days Worked");
+        System.out.println(remainingHours + " Remaining Hours");
 
-        IO.println(String.format("Hello and welcome!"));
-        System.out.println("Employee Name: " + employeeName);
-        System.out.println("Employee No: " + employeeNumber);
-        System.out.println("Checks In: " + checkIn);
-        System.out.println("Checks out: " + checkOut);
-        System.out.println("Worked Hours: " + workHours);
-        System.out.println("Salary: " + salary);
-        System.out.println("Gross Salary: " + grossSalary);
-        System.out.println("Net Salary: " + netSalary);
-
-        }
     }
+
+}
